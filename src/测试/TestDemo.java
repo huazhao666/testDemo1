47f5674d40c381f561db961c25faf8a368958945
花朝666 <2602474620@qq.com>
2021-03-06 13:42:04 +0800

    测试

---
+++ src/测试/TestDemo.java
@@ -0,0 +1,24 @@
+package 测试;
+
+import java.util.Arrays;
+
+/**
+ * Created with Intellij IDEA
+ * Description:
+ * User : 花朝
+ * Date : 2021-03-05
+ * Time : 12:58
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+       String[] str = new String[5];
+       String str1[] = new String[5];
+       int[] array = new int[5];
+       int array1[] = new int[5];
+       
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(str));
+        System.out.println(Arrays.toString(str1));
+    }
+}
